a0592d070fca71615ba14740534b1c4c77110642
girish <girishkumarkh@gmail.com>
2014-06-23 11:14:36 +0100

    Commit up to Ex23

--- ex.py
+++ ex.py
@@ -1,3 +1,9 @@
+# Import stuff
+# Used in Ex 13:
+from sys import argv
+# Used in Ex 17:
+from os.path import exists
+
 # Ex 1 - A Good First Program
 def ex1():
 	print "hello world"
@@ -176,26 +182,219 @@ def ex11():
 
 # Ex 12 - Prompting People
 def ex12():
-	pass
+	age = raw_input("How old are you ? ")
+	height = raw_input("How tall are you? ")
+	weight = raw_input("How much do you weigh? ")
+	print "So, you're %r old, %r tall and %r heavy." % (
+		age, height, weight)
 
-# Ex 13 - 
-def ex12():
-	pass
-
-# Ex 14 - 
+# Ex 13 - Parameters, Unpacking, Variables
 def ex13():
-	pass
+	# requires the following
+	# from sys import argv (imported at the start of this file)
+	script, first, second, third = argv
+	print "The script is called:", script
+	print "Your fisrt variable is:", first
+	print "Yout second variable is:", second
+	print "Your third variable is:", third
 
-# Ex 15 - 
+# Ex 14 - Prompting and Passing
 def ex14():
-	pass
+	# requires the following
+	# from sys import argv (imported at the start of this file)
+	script, user_name = argv
+	prompt = '> '
+	print " Hi %s, I'm the %s script." % (user_name, script)
+	print "I'd like to ask you a few questions."
+	print "Do you like me %s?" % user_name
+	likes = raw_input(prompt)
+	print "Where so you live %s?" % user_name
+	lives = raw_input(prompt)
+	print "What kind of computer do you have?"
+	computer = raw_input(prompt)
+	print """
+	Aright, so you said %r about liking me.
+	You live in %r. Not sure where that is.
+	And you have a %r computer. Nice.
+	""" % (likes, lives, computer)
 
-# Ex 16 - 
+# Ex 15 - Reading Files
 def ex15():
+	# requires the following
+	# from sys import argv (imported at the start of this file)
+	script, filename = argv
+	txt = open(filename)
+	print "Here's you file %r:" % filename
+	print txt.read()
+	print "Type the filename again:"
+	file_again = raw_input("> ")
+	txt_again = open(file_again)
+	print txt_again.read()
+
+# Ex 16 - Reading and Writing Files
+def ex16():
+	# Some important stuff
+	# close -- Closes the file. Like File->Save.. in your editor.
+	# read -- Reads the contents of the file. You can assign the result to a variable.
+	# readline -- Reads just one line of a text file.
+	# truncate -- Empties the file. Watch out if you care about the file.
+	# write(stuff) -- Writes stuff to the file.
+	#
+	# requires the following
+	# from sys import argv (imported at the start of this file)
+	script, filename = argv
+	print "We're going to erase %r." % filename
+	print "If you don't want that, hit CTRL-C (^C)."
+	print "If you do want that, hit RETURN."
+	raw_input("?")
+	print "Opening the file..."
+	target = open(filename, 'w')
+	print "Truncating the file. Goodbye!"
+	target.truncate()
+	print "Now I'm going to ask you for three lines."
+	line1 = raw_input("line 1: ")
+	line2 = raw_input("line 2: ")
+	line3 = raw_input("line 3: ")
+	print "I'm going to write these to the file."
+	target.write(line1)
+	target.write("\n")
+	target.write(line2)
+	target.write("\n")
+	target.write(line3)
+	target.write("\n")
+	print "And finally, we close it."
+	target.close()
+
+# Ex 17 - More Files
+def ex17():
+	# requires the following
+	# from sys import argv (imported at the start of this file)
+	# from os.path import exists (imported at the start of this file)
+	script, from_file, to_file = argv
+	print "Copying from %s to %s" % (from_file, to_file)
+	# we could do these two on one line too, how?
+	in_file = open(from_file)
+	indata = in_file.read()
+	print "The input file is %d bytes long" % len(indata)
+	print "Does the output file exist? %r" % exists(to_file)
+	print "Ready, hit RETURN to continue, CTRL-C to abort."
+	raw_input()
+	out_file = open(to_file, 'w')
+	out_file.write(indata)
+	print "Alright, all done."
+	out_file.close()
+	in_file.close()
+
+# Ex 18 - Names, Variables, Code, Functions
+def ex18():
+	# this one is like your scripts with argv
+	def print_two(*args):
+	    arg1, arg2 = args
+	    print "arg1: %r, arg2: %r" % (arg1, arg2)
+	# ok, that *args is actually pointless, we can just do this
+	def print_two_again(arg1, arg2):
+	    print "arg1: %r, arg2: %r" % (arg1, arg2)
+	# this just takes one argument
+	def print_one(arg1):
+	    print "arg1: %r" % arg1
+	# this one takes no arguments
+	def print_none():
+	    print "I got nothin'."
+	print_two("Zed","Shaw")
+	print_two_again("Zed","Shaw")
+	print_one("First!")
+	print_none()
+
+# Ex 19 - Functions and Variables
+def ex19():
+	def cheese_and_crackers(cheese_count, boxes_of_crackers):
+		print "You have %d cheeses!" % cheese_count
+		print "You have %d boxes of crackers!" % boxes_of_crackers
+		print "Man that's enough for a party!"
+		print "Get a blanket.\n"
+	print "we can just give the function numbers directly:"
+	cheese_and_crackers(20, 30)
+	print "OR, we can use variables form out script:"
+	amount_of_cheese = 10
+	amount_of_crackers = 50
+	cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+	print "we can even do math inside too:"
+	cheese_and_crackers(10 + 20, 5 + 6)
+	print "And we can combine the two, variables and math:"
+	cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+  
+# Ex 20 - Functions and Files
+def ex20():
+	# requires the following
+	# from sys import argv (imported at the start of this file)
+	script, input_file = argv
+	def print_all(f):
+		print f.read()
+	def rewind(f):
+		f.seek(0)
+	def print_a_line(line_count, f):
+		print line_count, f.readline()
+	current_file = open(input_file)
+	print "First let's print the whole file:\n"
+	print_all(current_file)
+	print "Now let's rewind, kind of like a tape."
+	rewind(current_file)
+	print "Let's print three lines:"
+	current_line = 1
+	print_a_line(current_line, current_file)
+	current_line = current_line + 1
+	print_a_line(current_line, current_file)
+	current_line = current_line + 1
+	print_a_line(current_line, current_file)
+
+# Ex 21 - Functions Can Return Something
+def ex21():
+	def add(a, b):
+		print "ADDING %d + %d" % (a, b)
+		return a + b
+	def subtract(a, b):
+		print "SUBTRACTING %d - %d" % (a, b)
+		return a - b
+	def multiply(a, b):
+		print "MULTIPLYING %d * %d" % (a, b)
+		return a * b
+	def divide(a, b):
+		print "DEVIDING %d / %d" % (a, b)
+		return a / b
+	print "Let's do some math with just functions!"
+	age = add(30,50)
+	height = subtract(78, 4)
+	weight = multiply(90, 2)
+	iq = divide(100, 2)
+	print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (
+		age, height, weight, iq)
+	# A puzzle for the extra credit, type it in anyway.
+	print "Here is the puzzle."
+	what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+	print "That becomes: ", what, "Can you do it hand?"
+
+# Ex 22 - What Do You Know So Far?
+def ex22():
 	pass
 
-# Ex 17 - 
-def ex16():
+# Ex 23 - Read Some Code
+def ex23():
+	pass
+
+# Ex 24 - 
+def ex24():
+	pass
+
+# Ex 25 - 
+def ex25():
+	pass
+
+# Ex 26 - 
+def ex26():
+	pass
+
+# Ex 27 - 
+def ex27():
 	pass
 
 # Testing function - used of testing some part of my code
@@ -207,7 +406,8 @@ def test():
 	# print "Hey %s there." % "you" 
 	# print " This is to print %r" % "print this no matter what."
 	# print "a " + "b" # we can also write the program like this but this is not the ethical way 
+
 # main function
 if __name__ == "__main__":
-	ex12()
+	ex21()
 	# test()
