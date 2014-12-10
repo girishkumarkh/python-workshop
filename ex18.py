2265be144a0f86a11211aa7947c3ca281001c919
girishkumarkh <girishkumarkh@gmail.com>
2014-12-10 22:20:35 +0000

    ex 18

--- ex18.py
+++ ex18.py
@@ -1,7 +1,10 @@
+# Exercise 18: Names, Variables, Code, Functions
+
 # this one is like your scripts with argv
 def print_two(*args):
-    arg1, arg2 = args
-    print "arg1: %r, arg2: %r" % (arg1, arg2)
+    print "args: %r" % (args,)
+    arg1, arg2, arg3 = args
+    print "arg1: %r, arg2: %r, arg3: %r" % (arg1, arg2, arg3)
 
 # ok, that *args is actually pointless, we can just do this
 def print_two_again(arg1, arg2):
@@ -16,7 +19,7 @@ def print_none():
     print "I got nothin'."
 
 
-print_two("Zed","Shaw")
+print_two("Zed","Shaw","News")
 print_two_again("Zed","Shaw")
 print_one("First!")
 print_none()
