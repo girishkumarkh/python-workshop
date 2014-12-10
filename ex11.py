bafea3cb2571754b5a297c86218013b35756ccc3
girishkumarkh <girishkumarkh@gmail.com>
2014-12-10 19:35:10 +0000

    ex 8 - 11

---
+++ ex11.py
@@ -0,0 +1,16 @@
+# Exercise 11: Asking Questions
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
+a = "girish"
+b = "Midhun"
+
+print a,
+print b
