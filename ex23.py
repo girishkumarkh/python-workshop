d7e23faf570c4abdd7a6210bc100f521ec338e40
girishkumarkh <girishkumarkh@gmail.com>
2015-02-01 00:46:40 +0000

    ex 23

---
+++ ex23.py
@@ -0,0 +1,9 @@
+# Exercise 23: Read Some Code
+
+"""
+Go to bitbucket.org, github.com, or gitorious.org with your favorite web browser and search for "python."
+Pick a random project and click on it.
+Click on the Source tab and browse through the list of files and directories until you find a .py file.
+Start at the top and read through the code, taking notes on what you think it does.
+If any symbols or strange words seem to interest you, write them down to research later.
+"""
