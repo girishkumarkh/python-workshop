849521d3423bcbbd9b2123878508aa6cc84fefd8
girishkumarkh <girishkumarkh@gmail.com>
2015-02-01 01:37:05 +0000

    ex 36

---
+++ ex36.py
@@ -0,0 +1,9 @@
+# Exercise 36: Designing and Debugging
+
+"""
+Every if-statement must have an else.
+If this else should never run because it doesn't make sense, then you must use a die function in the else that prints out an error message and dies, just like we did in the last exercise. This will find many errors.
+Never nest if-statements more than two deep and always try to do them one deep.
+Treat if-statements like paragraphs, where each if-elif-else grouping is like a set of sentences. Put blank lines before and after.
+Your boolean tests should be simple. If they are complex, move their calculations to variables earlier in your function and use a good name for the variable.
+"""
