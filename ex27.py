6511b1170ae155f18c7de5eb09de8cf64b22f0cb
girishkumarkh <girishkumarkh@gmail.com>
2015-02-01 00:47:43 +0000

    ex 27

---
+++ ex27.py
@@ -0,0 +1,45 @@
+# Exercise 27: Memorizing Logic
+"""
+The Truth Tables
+We will now use these characters to make the truth tables you need to memorize.
+
+NOT	TRUE?
+not False	True
+not True	False
+
+OR	TRUE?
+True or False	True
+True or True	True
+False or True	True
+False or False	False
+
+AND	TRUE?
+True and False	False
+True and True	True
+False and True	False
+False and False	False
+
+NOT OR	TRUE?
+not (True or False)	False
+not (True or True)	False
+not (False or True)	False
+not (False or False)	True
+
+NOT AND	TRUE?
+not (True and False)	True
+not (True and True)	False
+not (False and True)	True
+not (False and False)	True
+
+!=	TRUE?
+1 != 0	True
+1 != 1	False
+0 != 1	True
+0 != 0	False
+
+==	TRUE?
+1 == 0	False
+1 == 1	True
+0 == 1	False
+0 == 0	True
+"""
