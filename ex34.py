5ea074de78849d9ca56f24f3c7d54822f09ff433
girishkumarkh <girishkumarkh@gmail.com>
2015-02-01 01:36:53 +0000

    ex 34

---
+++ ex34.py
@@ -0,0 +1,24 @@
+# Exercise 34: Accessing Elements of Lists
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+"""
+The animal at 1.
+The third (3rd) animal.
+The first (1st) animal.
+The animal at 3.
+The fifth (5th) animal.
+The animal at 2.
+The sixth (6th) animal.
+The animal at 4.
+"""
+print animals[1]
+print animals[2]
+print animals[0]
+print animals[3]
+print animals[4]
+print animals[1]
+print animals[5]
+print animals[4]
