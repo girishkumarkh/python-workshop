5b82048b0a2991bd1a86a933b1c8c424cc846880
girishkumarkh <girishkumarkh@gmail.com>
2015-02-01 00:46:34 +0000

    ex 22

---
+++ ex22.py
@@ -0,0 +1,15 @@
+# Exercise 22: What Do You Know So Far?
+
+"""
+There won't be any code in this exercise or the next one, so there's no What You Should See or Study Drills either. In fact, this exercise is like one giant Study Drills. I'm going to have you do a review of what you have learned so far.
+
+First, go back through every exercise you have done so far and write down every word and symbol (another name for "character") that you have used. Make sure your list of symbols is complete.
+
+Next to each word or symbol, write its name and what it does. If you can't find a name for a symbol in this book, then look for it online. If you do not know what a word or symbol does, then read about it again and try using it in some code.
+
+You may run into a few things you can't find out or know, so just keep those on the list and be ready to look them up when you find them.
+
+Once you have your list, spend a few days rewriting the list and double-checking that it's correct. This may get boring but push through and really nail it down.
+
+Once you have memorized the list and what they do, then step it up by writing out tables of symbols, their names, and what they do from memory. If you hit some you can't recall from memory, go back and memorize them again.
+"""
